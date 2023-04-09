9f59bd14e8e0f38b07c5ee7b1602ef19b02cb064
Stepan <115095505+HappySaber@users.noreply.github.com>
2023-04-09 23:06:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -22,18 +22,24 @@ std::string infx2pstfx(std::string inf) {
     if ((inf[i] >= '0') && (inf[i] <= '9')) {
       s += inf[i];
       s += " ";
-    } else if (inf[i] == '(')
+    } else
+      if (inf[i] == '(')
       stack1.push(inf[i]);
-    else if (inf[i] == ')') {
-      while (!stack1.isEmpty() && getPrior(stack1.getValue()) >= getPrior(inf[i])) {
+    else
+      if (inf[i] == ')') {
+      while (!stack1.isEmpty() &&
+             getPrior(stack1.getValue()) >= getPrior(inf[i])) {
         s += stack1.getValue();
         s += " ";
         stack1.pop();
       }
       stack1.push(inf[i]);
     }
-    else if (inf[i] == '+' || inf[i] == '+' || inf[i] == '*' || inf[i] == '/') {
-      while (!stack1.isEmpty() && getPrior(inf[i]) <= getPrior(stack1.getValue())) {
+    else
+      if (inf[i] == '+' || inf[i] == '+' ||
+          inf[i] == '*' || inf[i] == '/') {
+      while (!stack1.isEmpty() &&
+             getPrior(inf[i]) <= getPrior(stack1.getValue())) {
         s += stack1.getValue();
         s += " ";
         stack1.pop();
@@ -60,18 +66,22 @@ int eval(std::string pref) {
     else if (pref[i] >= '0' && pref[i] <= '9') {
       k = pref[i] - '0';
       stack2.push(k);
-    } else if (pref[i] == '+' || pref[i] == '-' || pref[i] == '*' ||
-               pref[i] == '/') {
+    } else
+      if (pref[i] == '+' || pref[i] == '-' ||
+          pref[i] == '*' || pref[i] == '/') {
       l = stack2.getValue();
       stack2.pop();
       g = stack2.getValue();
       stack2.pop();
       if (pref[i] == '+') stack2.push(g + l);
-      else if (pref[i] == '-')
+      else
+        if (pref[i] == '-')
         stack2.push(g - l);
-      else if (pref[i] == '*')
+      else
+        if (pref[i] == '*')
         stack2.push(g * l);
-      else if (pref[i] == '/')
+      else
+        if (pref[i] == '/')
         stack2.push(g / l);
     }
         
