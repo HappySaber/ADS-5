d1193a4e0f8c4fe03c62cce7b64bb4e74b08f951
Stepan <115095505+HappySaber@users.noreply.github.com>
2023-04-09 22:36:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,11 +4,65 @@
 #include "tstack.h"
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  TStack<char, 100> stack1;
+  std::string s = "";
+  for (int i = 0; i < inf.length(); i++) {
+    if ((inf[i] >= '0') && (inf[i] <= '9')) {
+      s += inf[i];
+      s += " ";
+    } else if (inf[i] == '(')
+      stack1.push(inf[i]);
+    else if (inf[i] == ')') {
+      while (!stack1.isEmpty() && getPrior(stack1.getValue()) >= getPrior(inf[i])) {
+        s += stack1.getValue();
+        s += " ";
+        stack1.pop();
+      }
+      stack1.push(inf[i]);
+    }
+    else if (inf[i] == '+' || inf[i] == '+' || inf[i] == '*' || inf[i] == '/') {
+      while (!stack1.isEmpty() && getPrior(inf[i]) <= getPrior(stack1.getValue())) {
+        s += stack1.getValue();
+        s += " ";
+        stack1.pop();
+      }
+      stack1.push(inf[i]);
+    }
+
+  }
+  while (!stack1.isEmpty()) {
+    s += stack1.getValue();
+    s += " ";
+    stack1.pop();
+  }
+
+  return s;
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  TStack<int, 100> stack2;
+  int k, l ,g;
+  for (int i = 0; i < pref.length(); i++) {
+    if (pref[i] == ' ')
+      continue;
+    else if (pref[i] >= '0' && pref[i] <= '9') {
+      k = pref[i] - '0';
+      stack2.push(k);
+    } else if (pref[i] == '+' || pref[i] == '-' || pref[i] == '*' ||
+               pref[i] == '/') {
+      l = stack2.getValue();
+      stack2.pop();
+      g = stack2.getValue();
+      stack2.pop();
+      if (pref[i] == '+') stack2.push(g + l);
+      else if (pref[i] == '-')
+        stack2.push(g - l);
+      else if (pref[i] == '*')
+        stack2.push(g * l);
+      else if (pref[i] == '/')
+        stack2.push(g / l);
+    }
+        
+  }
+  return stack2.getValue();
 }
